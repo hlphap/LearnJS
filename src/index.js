f409619aeec0626c02805f42d95c6859852c77e6
PhapHL <longphappy@gmail.com>
2021-05-31 11:17:40 +0700

    Full code to Use bootstrap

---
+++ src/index.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const express = require("express");
+const morgan = require("morgan");
+const exphbs = require("express-handlebars");
+const app = express();
+const port = 3000;
+
+//Static file
+app.use(express.static(path.join(__dirname, "public")));
+
+//HTTP logger
+app.use(morgan("combined"));
+
+//Template engine
+app.engine(
+  "hbs",
+  exphbs({
+    extname: ".hbs",
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "/resources/views"));
+
+//App express
+app.get("/", (req, res) => {
+  res.render("home");
+});
+//App express
+app.get("/news", (req, res) => {
+  res.render("news");
+});
+
+app.listen(port, () => {});
